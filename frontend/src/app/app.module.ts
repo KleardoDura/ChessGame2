34a7f12846d3d49b78a14976f5f6432061af71e0
KleardoDura <111565373+KleardoDura@users.noreply.github.com>
2023-11-08 11:38:11 -0800

    no commnet

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './components/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, UserProfileComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
